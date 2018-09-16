fac827ff1f211cb6b0baac4bb10a95ef85d4382a
Bogdan <bogdankf1@gmail.com>
2018-09-16 20:42:42 +0300

    Integrated basic server-to-api requests setup

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const Koa = require('koa')
+const Router = require('koa-router')
+const cors = require('koa2-cors')
+const bodyParser = require('koa-bodyparser')
+const { hostname, port } = require('./constants/constants.js')
+const { getStocksList } = require('./api/stocks.js')
+
 const app = new Koa()
+const router = new Router()
 
-app.use(async ctx => {
-  ctx.body = 'Hello World'
-  console.log(`Koa is running on localhost:${3001}`)
-})
+router.get('/stocks/list', getStocksList)
 
-app.listen(3001)
+app.use(cors())
+app.use(bodyParser())
+app.use(router.routes())
+
+const run = async () => {
+  app.listen(port, () => {
+    console.log(`Koa server listening on http://${hostname}:${port}`)
+  })
+}
+
+run()
