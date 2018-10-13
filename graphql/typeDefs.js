7ce1ef1d47f171f55159c8b562a42ceb5d094426
Bogdan <bogdankf1@gmail.com>
2018-10-14 01:14:06 +0300

    Implemented company stocks chart loading

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -1,44 +1,6 @@
 const { gql } = require('apollo-server-koa')
 
 const typeDefs = gql`
-  # type StockItem {
-  #   index: {
-  #     quote: {
-  #       symbol: String,
-  #       companyName: String,
-  #       primaryExchange: String,
-  #       sector: String,
-  #       calculationPrice: String,
-  #       open: Float,
-  #       openTime: Int,
-  #       close: Float,
-  #       closeTime: Int,
-  #       high: Float,
-  #       low: Float,
-  #       latestPrice: Float,
-  #       latestSource: String,
-  #       latestTime: String,
-  #       latestUpdate: Int,
-  #       latestVolume: Int,
-  #       delayedPrice: Float,
-  #       delayedPriceTime: Int,
-  #       extendedPrice: Float,
-  #       extendedChange: Float,
-  #       extendedChangePercent: Float,
-  #       extendedPriceTime: Int,
-  #       previousClose: Float,
-  #       change: Float,
-  #       changePercent: Float,
-  #       avgTotalVolume: Int,
-  #       marketCap: Int,
-  #       peRatio: Float,
-  #       week52High: Float,
-  #       week52Low: Float,
-  #       ytdChange: Float
-  #     }
-  #   }
-  # }
-
   type SymbolItem {
     symbol: String,
     name: String,
@@ -47,17 +9,39 @@ const typeDefs = gql`
     type: String,
     iexId: String
   }
-
+  
   type SymbolsList {
     list: [SymbolItem]
   }
+
   type Stocks {
     list: String
+  }
+
+  type StocksChartItem {
+    date: String,
+    open: Float,
+    high: Float,
+    low: Float,
+    close: Float,
+    volume: Int,
+    unadjustedClose: Float,
+    unadjustedVolume: Int,
+    change: Float,
+    changePercent: Float,
+    vwap: Float,
+    label: String,
+    changeOverTime: Float
+  }
+
+  type StocksChart {
+    list: [StocksChartItem]
   }
 
   type Query {
     stocks(id: String): Stocks
     symbols: SymbolsList
+    stocksChart(id: String): StocksChart
   }
 `
 
